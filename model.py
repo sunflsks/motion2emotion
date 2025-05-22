1873cc079c96e7368fa3daaee45b8f2703e39cca
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2025-05-22 16:30:13 -0500

    update model arch

--- model.py
+++ model.py
@@ -8,8 +8,15 @@ class Transformer(nn.Module):
         # projects raw coordinates into the model's vector space (linear projection)
         self.tokenizer = nn.Linear(input_dim, d_model)
 
+        # normalize projection
+        self.input_norm = nn.LayerNorm(d_model)
+
+        # dropout
+        self.input_dropout = nn.Dropout(0.1)
+
         # classification token
         self.class_token = nn.Parameter(torch.randn(1, 1, d_model))
+        self.class_norm = nn.LayerNorm(d_model)
 
         # positional embeddings
         self.positional_encoding = nn.Parameter(torch.randn(1, seq_len + 1, d_model))
@@ -30,16 +37,26 @@ class Transformer(nn.Module):
 
     def forward(self, x, mask=None):
         # x.shape = (batch_size, seq_len, 36 // 18 x 2, 18 joints and X, Y dims // )
-        dev = mask.device
         batch_size = x.shape[0] # prob ~64
 
-        false_tensor = torch.tensor([False]).to(dev).expand(batch_size, -1)
-        mask = torch.cat([false_tensor, mask], dim=1)
+        if mask is not None:
+            dev = mask.device
+            false_tensor = torch.tensor([False]).to(dev).expand(batch_size, -1)
+            mask = torch.cat([false_tensor, mask], dim=1)
+
 
         x = self.tokenizer(x) # project into d-dimensional space
+        x = self.input_norm(x) # normalize
+        x = self.input_dropout(x)
+
         class_token = self.class_token.expand(batch_size, -1, -1) # get class token
         x = torch.cat([class_token, x], dim=1) # add class token to sequence
+
         x = x + self.positional_encoding[:, : x.shape[1], :] # add positional encodings
+
         x = self.transformer(x, src_key_padding_mask=mask) # transform
+
         x = x[:, 0, :] # isolate class token
+        x = self.class_norm(x)
+
         return self.output_layer(x)
